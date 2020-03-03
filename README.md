ca9e6599a06c657361db8710731691a7c156356b
Ejez <ejez@protonmail.ch>
2020-03-03 18:28:19 +0300

    chore(docs): Add vue-styleguidist

---
+++ README.md
@@ -0,0 +1,4 @@
+## Noor! The Vue frontend for any CMS
+
+Noor Allows you to quickly create the frontend of your CMS, it provides ready
+made Vue components for CMS models (content types) and their associated fields.
---
+++ styleguide.config.js
@@ -0,0 +1,61 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+module.exports = {
+  title: 'Noor Docs',
+
+  pagePerSection: true,
+
+  sections: [
+    {
+      name: 'Introduction',
+      content: 'README.md'
+    },
+    {
+      name: 'API Reference',
+      sections: [
+        {
+          name: 'Model components',
+          description: 'Noor model components',
+          components: 'src/components/NrModel*.vue'
+        },
+        {
+          name: 'Field components',
+          description: 'Noor field components',
+          components: 'src/components/NrField*.vue'
+        }
+      ]
+    }
+  ],
+
+  ribbon: {
+    url: 'https://github.com/ejez/noor',
+    text: 'Fork me on GitHub'
+  },
+
+  webpackConfig: {
+    module: {
+      rules: [
+        // Vue loader
+        {
+          test: /\.vue$/,
+          exclude: /node_modules/,
+          loader: 'vue-loader'
+        },
+        // Babel loader, will use your project’s .babelrc
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        // Other loaders that are needed for your components
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader'
+        }
+      ]
+    },
+    plugins: [
+      // add vue-loader plugin
+      new VueLoaderPlugin()
+    ]
+  }
+}
